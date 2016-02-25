fc77711e7e1674e087161ce90575b5431c383707
Don Denton <don@happycollision.com>
2016-02-25 16:54:34 -0600

    pass the first soft tab replace test

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var map = require('map-stream')
 
 module.exports = function harden(size) {
-  var hardTab = /\t/g
-    , softTab = new Array(size + 1).join(' ')
+  var hardTab = '\t'
+    , spaces = new Array(size + 1).join(' ')
+    , softTab = new RegExp('^((?:' + spaces + ')+)(.*)','g')
   
   return map(function(file, callback) {
     try {
-      file.contents = new Buffer(String(file.contents).replace(hardTab, softTab))
+      file.contents = new Buffer(String(file.contents).replace(softTab, function(wholeString, p1, p2){
+        var numTabs = p1.length / size
+        var tabs = ''
+        while (numTabs > 0) {
+          tabs += hardTab;
+          numTabs--
+        }
+        return tabs + p2;
+      }))
     } catch (e) {
       console.warn('Error: ' + e.message)
     }
